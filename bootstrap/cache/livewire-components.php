c77fec4bab6bce948268a14c17930b5a8a355d05
sacquiatenjosh <joshuabsacquiaten@gmail.com>
2025-05-09 08:05:20 +0800

    Add files via upload

---
+++ bootstrap/cache/livewire-components.php
@@ -0,0 +1,7 @@
+<?php return array (
+  'admin.farmers-data' => 'App\\Http\\Livewire\\Admin\\FarmersData',
+  'crops-table' => 'App\\Http\\Livewire\\CropsTable',
+  'secretary.secretary-farmers-table' => 'App\\Http\\Livewire\\Secretary\\SecretaryFarmersTable',
+  'staff.user-manage' => 'App\\Http\\Livewire\\Staff\\UserManage',
+  'farmers-table' => 'App\\Http\\Livewire\\farmerstable',
+);
